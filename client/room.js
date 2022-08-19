e8482923ac24c1b21b38625159828b64f0e242f6
Sahajpreet Singh <sp.code2003@gmail.com>
2022-08-19 13:49:58 +0530

    improved-code

--- client/room.js
+++ client/room.js
@@ -13,11 +13,9 @@ console.log("username = " + username)
 const writeEvent = (p) => {
     const parent = document.querySelector('#participants');
     parent.innerHTML = "";
-    console.log(p);
     for (var j = 0; j < p.length; j++) {
         const el = document.createElement('li');
         el.innerHTML = p[j];
-        console.log(p[j]);
         parent.appendChild(el);
     }
 };
@@ -31,6 +29,7 @@ const sock = io();
 document.querySelector('#yo').addEventListener('submit', onFormSubmitted);
 var timeleft;
 function do_change1() {
+    var r,s;
     document.getElementById("start").hidden = true;
     timeleft = 10;
     var Timer = setInterval(function () {
@@ -38,10 +37,11 @@ function do_change1() {
             document.getElementById("yo").style.display = "block";
             document.getElementById("countdown").innerHTML = "Finished";
             document.getElementById('gif').src = './images/Music.png';
-            document.getElementById("youtube-audio").click();
             clearInterval(Timer);
             document.getElementById("next").hidden = false;
             document.getElementById("chat").readOnly = true;
+            player.stopVideo();
+            
         } else {
             
             document.getElementById("countdown").innerHTML = timeleft + " seconds remaining";
@@ -51,35 +51,40 @@ function do_change1() {
         timeleft -= 1;
     }, 1000);
 
-    fetch('https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=50&playlistId=PLB03EA9545DD188C3&key=AIzaSyAO0QSijwnmESBZfxYexkXS3MHE509trLc')
+    fetch('https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=50&playlistId=PLhsz9CILh357zA1yMT-K5T9ZTNEU6Fl6n&key=AIzaSyAO0QSijwnmESBZfxYexkXS3MHE509trLc')
   .then( response => {
     if (!response.ok) {
       throw new Error(`HTTP error: ${response.status}`);
     }
-    return response.text();
+    return response.json();
   })
-  .then( text =>{
-      var r=text.indexOf('videoId');
-      var s = text.substring(r+11,r+11+11);
-        console.log(s);
-        localStorage.setItem("s", s);
-        document.getElementById("youtube-audio").click();
-
+  .then( data =>{
+       console.log(console.log(data.items[0].snippet.resourceId.videoId));
+       console.log(data.items[0].snippet.title);
+       document.getElementById("videoId").innerHTML = console.log(data.items[0].snippet.resourceId.videoId)
+        document.getElementById("trackTitle").innerHTML = data.items[0].snippet.title
+        localStorage.setItem("position", 0);
+        player.playVideo();
   })
+  
 };
+var vid;
 function do_change2() {
-
+    var r,s;
+    l = parseInt(localStorage.getItem('position'));
+    localStorage.setItem('position', l+1 );
     document.getElementById("next").hidden = true;
     timeleft = 10;
     Timer = setInterval(function () {
         if (timeleft <= 0) {
             document.getElementById("yo").style.display = "block";
             document.getElementById("countdown").innerHTML = "Finished";
-            document.getElementById("youtube-audio").click();
             clearInterval(Timer);
             document.getElementById("next").hidden = false;
             document.getElementById('gif').src = './images/Music.png';
             document.getElementById("chat").readOnly = true;
+            player.stopVideo();
+            
         } else {
             document.getElementById("countdown").innerHTML = timeleft + " seconds remaining";
             document.getElementById("chat").readOnly = false;
@@ -87,86 +92,71 @@ function do_change2() {
         }
         timeleft -= 1;
     }, 1000);
-    next();
+    fetch('https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&maxResults=50&playlistId=PLhsz9CILh357zA1yMT-K5T9ZTNEU6Fl6n&key=AIzaSyAO0QSijwnmESBZfxYexkXS3MHE509trLc')
+  .then( response => {
+
+    if (!response.ok) {
+      throw new Error(`HTTP error: ${response.status}`);
+    }
+    return response.json();
+  })
+  .then( data =>{
+
+    console.log(console.log(data.items[localStorage.getItem('position')].snippet.resourceId.videoId));
+    console.log(data.items[localStorage.getItem('position')].snippet.title);
+    document.getElementById("videoId").innerHTML = data.items[localStorage.getItem('position')].snippet.resourceId.videoId
+    document.getElementById("trackTitle").innerHTML = data.items[localStorage.getItem('position')].snippet.title
+     player.playVideo();
+    })
+
+  
 };
 
 
-function onYouTubeIframeAPIReady(){
+ // 2. This code loads the IFrame Player API code asynchronously.
+ var tag = document.createElement('script');
 
-    var ctrlq=document.getElementById("youtube-audio");
-    
-    // var icon=document.createElement("img");
-    // icon.setAttribute("id","youtube-icon"),
-    // icon.style.cssText="cursor:pointer;cursor:hand";
-    // ctrlq.appendChild(icon);
+ tag.src = "https://www.youtube.com/iframe_api";
+ var firstScriptTag = document.getElementsByTagName('script')[0];
+ firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-    var div=document.createElement("div");
-    div.setAttribute("id","youtube-player");
-    ctrlq.appendChild(div);
-    //https://i.imgur.com/IDzX9gL.png
-    //https://i.imgur.com/quyUPXN.png
-    
-// var toggleButton = function(play){
-//     var img = play ? "IDzX9gL.png":"quyUPXN.png";
-//     icon.setAttribute("src","https://i.imgur.com/"+img)
-// };
+ // 3. This function creates an <iframe> (and YouTube player)
+ //    after the API code downloads.
+ var player;
+ function onYouTubeIframeAPIReady() {
+   player = new YT.Player('player', {
+     height: '390',
+     width: '640',
+     videoId: document.getElementById("videoId").innerHTML,
+     playerVars: {
+       'playsinline': 1
+     },
+     events: {
+       'onReady': onPlayerReady,
+       'onStateChange': onPlayerStateChange
+     }
+   });
+ }
 
-ctrlq.onclick=function(){
-    if(player.getPlayerState()===YT.PlayerState.PLAYING
-    ||player.getPlayerState()===YT.PlayerState.BUFFERING){
-        player.pauseVideo();
-        // toggleButton(false);
-    }else{
-        player.playVideo();
-        // toggleButton(true);
-    }
-};
+ // 4. The API will call this function when the video player is ready.
+ function onPlayerReady(event) {
+   event.target.playVideo();
+ }
 
-var player=new YT.Player("youtube-player",{
-    height:"0",width:"0",
-    videoId: localStorage.getItem('s'),
-    playerVars:{
-        autoplay:ctrlq.dataset.autoplay,
-        loop:ctrlq.dataset.loop
-    },
-    events:{
-        'onReady':function(e){
-            player.setPlaybackQuality("small"),
-            toggleButton(player.getPlayerState()!== YT.PlayerState.CUED)
-        },
-        'onStateChange':function(e){
-            if(e.data === YT.PlayerState.ENDED){
-                toggleButton(false);
-            }
-            
-        }
-    }
-}
-)};
+ // 5. The API calls this function when the player's state changes.
+ //    The function indicates that when playing a video (state=1),
+ //    the player should play for six seconds and then stop.
+ var done = false;
+ function onPlayerStateChange(event) {
+   if (event.data == YT.PlayerState.PLAYING && !done) {
+     setTimeout(stopVideo, 6000);
+     done = true;
+   }
+ }
+ function stopVideo() {
+   player.stopVideo();
+ }
 
-
-
-
-
-
-function onPageLoad() {
-    if (window.location.search.length > 0) {
-        handleRedirect();
-    }
-    else {
-        access_token = localStorage.getItem("access_token");
-        if (access_token == null) {
-            // we don't have an access token so present token section
-            document.getElementById("tokenSection").style.display = 'block';
-        }
-        else {
-            // we have an access token so present device section
-            document.getElementById("deviceSection").style.display = 'block';
-            refreshDevices();
-            currentlyPlaying();
-        }
-    }
-}
 
 
 const writeAns = (text) => {
@@ -181,7 +171,7 @@ const Answered = (e) => {
     e.preventDefault();
     const input = document.querySelector('#chat');
     const text = input.value;
-    if (text == document.getElementById("trackTitle").innerHTML) {
+    if (document.getElementById("trackTitle").innerHTML.toLowerCase().includes(text)) {
         var t = scores[idx];
         t = parseInt(t) + 100;
         scores[idx] = t;
